d9e346b301cf1fe0d1367e168ae5b151dd96cf17
Jenn Schiffer <jenn@dotbiz.info>
2017-03-28 14:30:39 -0400

    init commit

---
+++ config/environment.js
@@ -0,0 +1,10 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'glimmer-hello-world',
+    environment: environment
+  };
+
+  return ENV;
+};
